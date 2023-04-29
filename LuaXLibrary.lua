c1a8bfd802344c6493dd6941bb495a045744c801
Pluto420Top <98620650+Pluto420Top@users.noreply.github.com>
2023-04-29 03:48:15 -0500

    Update LuaXLibrary.lua

--- LuaXLibrary.lua
+++ LuaXLibrary.lua
@@ -1,68 +1,38 @@
+local Library = {}
+-----------[['Library']]--------------------
+
+local LocalPlayer = game:GetService('Players').LocalPlayer
+local PlayerGui = LocalPlayer.PlayerGui
+-------------------['Player Things']--------
+
 local UIS = game:GetService('UserInputService')
-local Players = game:GetService('Players')
-local LocalPlayer = Players.LocalPlayer
-------------------[['Services']]-----------------
+-------------------['Services']--------------
 
-local LuaX = nil
-local frame = nil
-local dragToggle = nil
-local dragSpeed = 0.1
-local dragStart = nil
-local startPos = nil
-local A = 0
-local library = {}
+local ScreenGui = nil
+local Frame = nil
+-------------------['UI Things']-------------
 
-function library:CreateWindow(Name, LuaX, Frame)
-	 local LuaX = Instance.new('ScreenGui')
-	 local Frame = Instance.new('Frame')
-	 local UICorner = Instance.new('UICorner')
-	 Frame.Parent = LuaX
-	 frame = Frame
-	 UICorner.Parent = Frame
-	
-	 frame.AnchorPoint = Vector2.new(0.5, 0.5)
-	 frame.Position = UDim2.new(0.5, 0, 0.5, 0)
-	 frame.Size = UDim2.new(0, 561, 0, 331)
-	 frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-	 LuaX.Parent = LocalPlayer.PlayerGui
-	 A = A + 1
-	 LuaX.Name = "LuaX"
-	 frame.Name = "LuaXFrame"
-	 LuaX.IgnoreGuiInset = true
-	if A == 2 then
-		LuaX:Destroy()
-		if LuaX == nil then
-			A = 0
-		end
-	end
+
+function Library:CreateWindow(WindowName)
+	local ScreenGui = Instance.new('ScreenGui', PlayerGui)
+	ScreenGui.Name = WindowName	
+	ScreenGui.IgnoreGuiInset = true
 end
 
 
 
-local function updateInput(input)
-	local delta = input.Position - dragStart
-	local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-		startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+
+function Library:CreateTab(TabName)
+	local Frame = Instance.new('Frame', ScreenGui)
+	local UICorner = Instance.new('UICorner', Frame)
+	Frame.Size = UDim2.new(0, 561, 0, 341)
+	Frame.AnchorPoint = Vector2.new(0.5, 0.5)
+	Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
+	Frame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+	Frame.Name = TabName
 end
 
-frame.InputBegan:Connect(function(input)
-	if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
-		dragToggle = true
-		dragStart = input.Position
-		startPos = frame.Position
-		input.Changed:Connect(function()
-			if input.UserInputState == Enum.UserInputState.End then
-				dragToggle = false
-			end
-		end)
-	end
-end)
 
-UIS.InputChanged:Connect(function(input)
-	if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-		if dragToggle then
-			updateInput(input)
-		end
-	end
-end)
+--function Library:CreateTextButton(ButtonName)
+	--local Button = Instance.new('TextButton', Frame)
+--end
